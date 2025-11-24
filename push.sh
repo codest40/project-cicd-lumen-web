b9e6c10cf5db07b9975b74d1d6df0348035f7611
codest40 <generalcatmouse@gmail.com>
2025-11-24 10:18:24 +0100

    New Commit Number

--- push.sh
+++ push.sh
@@ -1,3 +1,11 @@
 
-if 
+
+if [ -z "$1" ]; then
+  $1="None"
+  echo "Empty Imput for $0"
+  echo "Default input now is $1"
+else
+  echo "Imput $1 is given for $0"
+fi
+
 git add . && git commit -m "New Commit Number $1" && git push
