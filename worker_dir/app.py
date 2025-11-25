17a580b58419538d25bf4695f4449540f3f937a7
codest40 <generalcatmouse@gmail.com>
2025-11-25 15:44:27 +0100

    New Commit Number 2025-11-25 15:44:27

--- worker_dir/app.py
+++ worker_dir/app.py
@@ -14,11 +14,6 @@ def start_worker():
     print("[FLASK] Starting background worker thread...")
     asyncio.run(worker_loop())
 
-if __name__ == "__main__":
-    print("[FLASK] Starting app.py main...")
 
-    # Start worker in background thread
-    threading.Thread(target=start_worker, daemon=True).start()
+threading.Thread(target=start_worker, daemon=True).start()
 
-    #print("[FLASK] Starting Flask server...")
-    app.run(host="0.0.0.0", port=5000)
